0a6dfe0790d4bab3ff928de3227ff10635cf05a5
Natasha Buckham <natashaisobel@gmail.com>
2024-08-05 10:56:34 +0100

    Prints Fizz for multiples of 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (var counter = 1; counter < 101; counter++)
 {
-    Console.WriteLine(counter);
+    if (counter % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    } else
+    {
+        Console.WriteLine(counter);
+    }
 }
