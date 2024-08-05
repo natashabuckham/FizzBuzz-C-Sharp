06138a2d852b279230d909320d98c99049e8d144
Natasha Buckham <natashaisobel@gmail.com>
2024-08-05 10:59:49 +0100

    Adds Buzz and FizzBuzz

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (var counter = 1; counter < 101; counter++)
 {
-    if (counter % 3 == 0)
+    if (counter % 3 == 0 && counter % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (counter % 3 == 0)
     {
         Console.WriteLine("Fizz");
-    } else
+    } 
+    else if (counter % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
     {
         Console.WriteLine(counter);
     }
