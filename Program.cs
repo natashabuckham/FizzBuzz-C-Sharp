61f5c1df80d76d1e686680c013182c0ad5d28ba8
Natasha Buckham <natashaisobel@gmail.com>
2024-08-05 10:53:44 +0100

    Prints every number up to 100

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (var counter = 1; counter < 101; counter++)
+{
+    Console.WriteLine(counter);
+}
